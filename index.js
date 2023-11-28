b8b0b521b727381ef3fcfa8a52e9c6bbd857995e
Pablo <pablomurbaez@yahoo.com>
2023-11-28 00:41:06 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const name = "Susan";
-const height = 20;
+const height = 39;
 const message = `${name} is ${height} inches tall`;
 
-module.exports = {name, height, message};
+module.exports = { name, height, message};
+console.log(message);
